556d9a3ba6b57fcc5b2299768bfbaba4a76ecf98
lynsvnxk0708 <nguyennhatlinh.l.15cla@gmail.com>
2021-12-09 22:02:13 +0700

    add html file

---
+++ .history/trang_chu_20211208163443.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>
+
+    <?php 
+      $a = 5;
+      if($a < 10){
+        echo "Biến a nhỏ hơn 10";
+      }else{
+        echo "Biến a lớn hơn hoặc bằng 10";
+      }
+    ?>
+  </h1>
+  </body>
+</html>
